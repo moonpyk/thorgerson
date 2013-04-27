fed129d9f6970c6909d25346b745762ad83c1c18
Clément Bourgeois <moonpyk@gmail.com>
2013-04-27 22:26:02 +0200

    AutoInstaller class on AppStart

---
+++ Thorgerson/App_Start/AutoInstall.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Security;
+using Thorgerson.Models;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Thorgerson.AutoInstall), "PostStart")]
+
+namespace Thorgerson
+{
+    public class AutoInstall
+    {
+        public static void PostStart()
+        {
+            using (var ddb = new ThorgersonContext())
+            {
+                ddb.Database.Initialize(false);
+            }
+
+            MembershipUser defaultUser = null;
+            if (Membership.GetAllUsers().Count == 0)
+            {
+                defaultUser = Membership.CreateUser("admin", "password");
+            }
+
+            if (!Roles.RoleExists("Admin"))
+            {
+                Roles.CreateRole("Admin");
+
+                if (defaultUser != null)
+                {
+                    Roles.AddUserToRole(defaultUser.UserName, "Admin");
+                }
+            }
+        }
+    }
+}
---
+++ Thorgerson/Models/ThorgersonContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+
+namespace Thorgerson.Models
+{
+    public class ThorgersonContext : DbContext
+    {
+        // Vous pouvez ajouter du code personnalisé à ce fichier. Les modifications ne seront pas remplacées.
+        // 
+        // Si vous voulez qu’Entity Framework abandonne et régénère la base de données
+        // automatiquement à chaque fois que vous modifiez le schéma du modèle, ajoutez le code
+        // suivant à la méthode Application_Start dans le fichier Global.asax.
+        // Remarque : cette opération supprime et recrée la base de données à chaque modification du modèle.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<Thorgerson.Models.ThorgersonContext>());
+
+        static ThorgersonContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ThorgersonContext>());
+        }
+
+        public ThorgersonContext()
+            : base("name=ThorgersonContext")
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
